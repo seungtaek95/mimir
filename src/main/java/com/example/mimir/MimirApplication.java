0f75cb94ff5ed403b2f43e2d61e1d0a443091e96
seungtaek95 <seungtaek95@gmail.com>
2022-08-06 21:59:29 +0900

    initial commit

---
+++ src/main/java/com/example/mimir/MimirApplication.java
@@ -0,0 +1,13 @@
+package com.example.mimir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimirApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MimirApplication.class, args);
+    }
+
+}
