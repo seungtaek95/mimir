22ce51d2f56f1bcd2e4a7cfb9a2c525b0b20b3c5
seungtaek95 <seungtaek95@gmail.com>
2022-12-12 00:26:28 +0900

    게시글 저장 테스트 작성

--- src/main/java/com/example/mimir/article/domain/entity/Article.java
+++ src/main/java/com/example/mimir/article/domain/entity/Article.java
@@ -1,21 +1,31 @@
 package com.example.mimir.article.domain.entity;
 
 import java.time.LocalDateTime;
-import java.util.Objects;
 import java.util.UUID;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.relational.core.mapping.Column;
+import com.example.mimir.common.util.UuidUtils;
 import com.example.mimir.member.domain.entity.Member;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+
+@Getter
 public class Article {
 	@Id
 	private Long id;
 
 	/** 작성자 id */
-	private UUID memberId;
+	@Column("member_id")
+	private final byte[] memberId;
+
+	@Transient
+	@Getter(value = AccessLevel.NONE)
+	private UUID _memberId;
 
 	/** 제목 */
 	private String title;
@@ -23,7 +33,7 @@ public class Article {
 	/** 내용 */
 	private String content;
 
-	/** 조회수 */
+	/** 비공개여부 */
 	@Column("is_private")
 	private boolean isPrivate;
 
@@ -34,7 +44,7 @@ public class Article {
 	/** 생성 년월일시 */
 	@CreatedDate
 	@Column("created_at")
-	private LocalDateTime createdAt;
+	private final LocalDateTime createdAt;
 
 	/** 수정 년월일시 */
 	@LastModifiedDate
@@ -46,11 +56,20 @@ public class Article {
 	private LocalDateTime deletedAt;
 
 	public Article(Member member, String title, String content, boolean isPrivate) {
-		this.memberId = Objects.requireNonNull(member).getId();
+		this.memberId = UuidUtils.uuidToBytes(member.getId());
 		this.title = title;
 		this.content = content;
 		this.isPrivate = isPrivate;
 		this.viewCount = 0;
 		this.createdAt = LocalDateTime.now();
+		this.updatedAt = LocalDateTime.now();
+	}
+
+	public UUID getMemberId() {
+		if (_memberId == null) {
+			_memberId = UuidUtils.bytesToUuid(memberId);
+		}
+
+		return _memberId;
 	}
 }
--- src/test/java/com/example/mimir/member/fixture/MemberFixture.java
+++ src/test/java/com/example/mimir/member/fixture/MemberFixture.java
@@ -4,8 +4,8 @@ import com.example.mimir.member.domain.entity.Member;
 
 public class MemberFixture {
 	public static final String email = "test@mimir.com";
-	public static final String password = "seungtaek";
-	public static final String nickname = "seungtaek";
+	public static final String password = "test";
+	public static final String nickname = "test";
 
 	public static Member create() {
 		return Member.signup(email, password, nickname);
